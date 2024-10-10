f34d8ffe84e2baf445c4d3a61b8b7091a213c8dd
Yaroslav Muravskyi <myarik@gmail.com>
2024-10-10 14:06:17 +0200

    Fix unit tests for demo_lambda handler

--- tests/service/test_base_lambda_function.py
+++ tests/service/test_base_lambda_function.py
@@ -1,6 +1,13 @@
+import pytest
+
 from service.handlers.demo_lambda import lambda_handler
 
 
 def test_lambda_handler():
-    event, context = {}, {}
-    assert lambda_handler(event, context) == {"message": "Hello from Lambda!"}
+    event, context = {"title": "Test", "content": "Test message"}, {}
+    response = lambda_handler(event, context)
+    assert "Test" in response
+    assert "Test message" in response
+
+    with pytest.raises(Exception):
+        lambda_handler({"title": "Te", "content": "message"}, {})
